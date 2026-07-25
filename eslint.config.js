6ebe9ada1108131f647ce1f4ca9aae8a3c8d71f0
coronelhernan <hcoronel1996@gmail.com>
2026-07-25 20:57:20 -0300

    chore: configurar reglas de estilo mediante @stylistic

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import stylistic from '@stylistic/eslint-plugin'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
@@ -15,8 +16,27 @@ export default defineConfig([
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
+    plugins: {
+      '@stylistic': stylistic,
+    },
     languageOptions: {
       globals: globals.browser,
+    },
+    rules: {
+      // Error si hay semicolons (estilo sin punto y coma)
+      '@stylistic/semi': ['error', 'never'],
+      // Comillas simples
+      '@stylistic/quotes': ['error', 'single', { avoidEscape: true }],
+      // Indentación de 2 espacios
+      '@stylistic/indent': ['error', 2],
+      // Espacios en llaves de objetos { foo }
+      '@stylistic/object-curly-spacing': ['error', 'always'],
+      // Sin trailing spaces
+      '@stylistic/no-trailing-spaces': 'error',
+      // Salto de línea final
+      '@stylistic/eol-last': ['error', 'always'],
+      // No variables sin usar (TS)
+      '@typescript-eslint/no-unused-vars': 'warn',
     },
   },
 ])
