ce32df0b7a3bd9bb86f5ab9aeecbe31c20758af2
coronelhernan <hcoronel1996@gmail.com>
2026-07-25 23:41:46 -0300

    feat: actualizar main.tsx integrando MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
 import './App.css'
+import { Container, Stack, Typography } from '@mui/material'
+import { JavaScriptLogo } from './components/JavaScriptLogo'
 
 function App() {
   return (
-    <>
-      <h1>Javascript Quizz</h1>
-    </>
+    <main>
+      <Container maxWidth="sm">
+        <Stack
+          sx={{
+            flexDirection: 'row',
+            gap: 2,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <JavaScriptLogo />
+          <Typography variant="h2" component="h1">
+            JavaScript Quizz
+          </Typography>
+        </Stack>
+      </Container>
+    </main>
   )
 }
 
