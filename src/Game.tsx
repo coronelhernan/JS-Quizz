bfa7509a810c4643d74d3fb6c04ef32ede603fc9
coronelhernan <hcoronel1996@gmail.com>
2026-08-01 23:50:09 -0300

    feat: instalar SyntaxHighLigther e integrarlo en Game.tsx

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,11 +1,17 @@
 import { Card, Typography } from '@mui/material'
 import { useQuestionsStore } from './store/questions'
 import { type Question as QuestionType } from './types.d'
+import SyntaxHighLigther from 'react-syntax-highlighter'
+import { monokai } from 'react-syntax-highlighter/dist/esm/styles/hljs'
 
 const Question = ({ info }: { info: QuestionType }) => {
   return (
-    <Card variant="outlined">
+    <Card variant="outlined" sx={{ bgcolor: '#222', p: 2, textAlign: 'left' }}>
       <Typography variant="h5">{info.question}</Typography>
+
+			<SyntaxHighLigther language='javascript' style={monokai}>
+				{info.code}
+			</SyntaxHighLigther>
     </Card>
   )
 }
